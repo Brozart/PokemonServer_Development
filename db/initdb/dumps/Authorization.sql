f81467d41602f3fc157852772aadfa0936a6b25c
Tom Van den Broeck <tom-vdbroeck@hotmail.com>
2018-11-25 20:40:27 +0100

    Add email to authorization user

--- db/initdb/dumps/Authorization.sql
+++ db/initdb/dumps/Authorization.sql
@@ -6,3 +6,13 @@ CREATE TABLE Client (
 );
 
 INSERT INTO Client (id, clientId, clientSecret, scope) VALUES (1, 'pokeReact', '$2a$10$mfjrN22bD325JJ0W56j.l.PnO3apKfpVTATjqOshfpsQQmgTA4gFa', 'pokemon_server');
+
+CREATE TABLE User(
+id BIGINT(20) PRIMARY KEY,
+username VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+enabled TINYINT(1) NOT NULL DEFAULT TRUE
+);
+
+INSERT INTO User (id, username, password, email, enabled) VALUES (1, 'user', '$2a$10$LSaFawSH0E37U.0g1q.zsuXSq2GKX8HOzZlstdgt4AeyJlVk6eRvi', 'tom-vdbroeck@hotmail.com', 1);
